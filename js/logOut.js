298eb7cf1d7577fca24f1ca55af03dfdbc5120f4
AleSaturno <80866665+AleSaturno@users.noreply.github.com>
2021-07-20 18:59:54 -0300

    Add files via upload

---
+++ js/logOut.js
@@ -0,0 +1,34 @@
+const buttonOut = document.getElementById("logOut");
+
+buttonOut.addEventListener("click",() => {
+    Swal.fire({
+        icon:'question',
+        title: 'Estas Seguro?',
+        showCancelButton: true,
+        confirmButtonText: 'Okay',
+        confirmButtonColor: '#3085d6',
+        cancelButtonText: 'Cancelar',
+        cancelButtonColor: '#d33',
+    }).then(result => {
+        if (result.isConfirmed) {
+            logOut()
+            return
+        }
+        return
+    })
+})
+
+function logOut(){
+    localStorage.removeItem("auth")
+    Swal.fire({
+        icon: 'success',
+        title: 'Vuelva Pronto',
+        showConfirmButton: false,
+        timer: 2000 
+    });
+    setTimeout (()=>{
+        window.location.href = "index.html"
+    },2000)
+}
+
+
